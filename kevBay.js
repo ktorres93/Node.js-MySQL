6e3965afdbe6424f908c18d236d94b73300e4c42
Kevin <ktorres93@knights.ucf.edu>
2017-06-24 14:47:53 -0400

    added dependencies and basic js file. working on server now.

---
+++ kevBay.js
@@ -0,0 +1,8 @@
+var inquirer = require('inquirer');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'me',
+    password : 'secret',
+    database : 'my_db'
+});
